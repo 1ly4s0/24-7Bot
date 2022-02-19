44c5c910575d326adcef15acd8098301caf31ac1
1ly4s0 <67460389+1ly4s0@users.noreply.github.com>
2022-02-19 20:20:47 +0100

    Create codigo.js

---
+++ codigo.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const server = express();
+ 
+server.all('/', (req, res) => {
+    res.send('Bot 24/7);
+});
+  
+server.listen(3000, () => {
+    console.log('Servidor Listo.');
+ });
